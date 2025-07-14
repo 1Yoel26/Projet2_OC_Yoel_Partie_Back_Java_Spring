a5845c452a197139cc443ca1094610ffd74cd8e1
= <=>
2025-07-14 13:45:25 +0200

    mon premier commit pour mon back sur github

---
+++ src/main/java/com/projet2_oc_y/projet2_oc_y/Projet2OcYApplication.java
@@ -0,0 +1,13 @@
+package com.projet2_oc_y.projet2_oc_y;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet2OcYApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet2OcYApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet2_oc_y/projet2_oc_y/Projet2OcYApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet2_oc_y.projet2_oc_y;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet2OcYApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
